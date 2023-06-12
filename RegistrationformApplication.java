c5b517cbb7b42dc20e4e58a669004d29f861cf92
Vana Niranjan <113905277+Vananiranjanarao@users.noreply.github.com>
2023-06-12 14:39:03 +0530

    Add files via upload

---
+++ RegistrationformApplication.java
@@ -0,0 +1,16 @@
+RegistrationformApplication.java
+
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationformApplication.class, args);
+		System.out.println("spring boot Appilication started");
+	}
+
+}
